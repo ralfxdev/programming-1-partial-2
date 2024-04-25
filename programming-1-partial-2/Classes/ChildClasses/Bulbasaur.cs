86f70b9e60537efe704275432014ef7a28d6e540
ralfxdev <lopezalexander1019@gmail.com>
2024-04-24 22:53:39 -0600

    Added parent class and child classes

---
+++ programming-1-partial-2/Classes/ChildClasses/Bulbasaur.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using programming_1_partial_2.Classes;
+
+namespace programming_1_partial_2.Classes.ChildClasses
+{
+    internal class Bulbasaur : Pokemon
+    {
+        public Bulbasaur(string name, int level, int health, int attack, int defense, int speed, int experience, int requiredExperience) : base(name, level, health, attack, defense, speed, experience, requiredExperience)
+        {
+        }
+
+        private void VineWhip(Pokemon enemy)
+        {
+            int damage = Attack * 2 - enemy.Defense;
+            if (damage < 0)
+            {
+                damage = 0;
+            }
+            enemy.Health -= damage;
+        }
+
+        private void RazorLeaf(Pokemon enemy)
+        {
+            int damage = Attack - enemy.Defense;
+            if (damage < 0)
+            {
+                damage = 0;
+            }
+            enemy.Health -= damage;
+        }
+
+        private void SolarBeam(Pokemon enemy)
+        {
+            int damage = Attack * 3 - enemy.Defense;
+            if (damage < 0)
+            {
+                damage = 0;
+            }
+            enemy.Health -= damage;
+        }
+
+        public override void AttackEnemy(Pokemon enemy)
+        {
+            Random random = new Random();
+            int attack = random.Next(1, 4);
+            switch (attack)
+            {
+                case 1:
+                    VineWhip(enemy);
+                    break;
+                case 2:
+                    RazorLeaf(enemy);
+                    break;
+                case 3:
+                    SolarBeam(enemy);
+                    break;
+            }
+        }
+    }
+}
---
+++ programming-1-partial-2/Classes/Pokemon.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programming_1_partial_2.Classes
+{
+    internal class Pokemon
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int Health { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Speed { get; set; }
+        public int Experience { get; set; }
+        public int RequiredExperience { get; set; }
+
+        public Pokemon(string name, int level, int health, int attack, int defense, int speed, int experience, int requiredExperience)
+        {
+            Name = name;
+            Level = level;
+            Health = health;
+            Attack = attack;
+            Defense = defense;
+            Speed = speed;
+            Experience = experience;
+            RequiredExperience = requiredExperience;
+        }
+
+        public void LevelUp()
+        {
+            Level++;
+            Health += 10;
+            Attack += 5;
+            Defense += 5;
+            Speed += 5;
+            RequiredExperience += 10;
+        }
+
+        public void GainExperience(int experience)
+        {
+            Experience += experience;
+            if (Experience >= RequiredExperience)
+            {
+                LevelUp();
+            }
+        }
+
+        public void Heal()
+        {
+            Health = 100;
+        }
+
+        public string ShowInfo()
+        {
+            return $"Name: {Name}\nLevel: {Level}\nHealth: {Health}\nAttack: {Attack}\nDefense: {Defense}\nSpeed: {Speed}\nExperience: {Experience}\nRequired Experience: {RequiredExperience}";
+        }
+
+        public virtual void AttackEnemy(Pokemon enemy)
+        {
+            int damage = Attack - enemy.Defense;
+            if (damage < 0)
+            {
+                damage = 0;
+            }
+            enemy.Health -= damage;
+        }
+    }
+}
